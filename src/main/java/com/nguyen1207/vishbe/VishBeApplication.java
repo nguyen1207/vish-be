1b748c8ea410c2da2ece67c32bbaed0397fceb99
Nguyen Hoang Nguyen <hoangnguyen789a@gmail.com>
2023-01-15 23:01:02 +0700

    Initial commit

---
+++ src/main/java/com/nguyen1207/vishbe/VishBeApplication.java
@@ -0,0 +1,13 @@
+package com.nguyen1207.vishbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VishBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VishBeApplication.class, args);
+    }
+
+}
