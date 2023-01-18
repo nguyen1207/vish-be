af64c9a6365e6486d4aae8905d113df2e536b72b
Nguyen Hoang Nguyen <hoangnguyen789a@gmail.com>
2023-01-18 22:32:41 +0700

    chore: change composite key from uuid to string

--- src/main/java/com/nguyen1207/vishbe/entities/invoice/InvoiceLineId.java
+++ src/main/java/com/nguyen1207/vishbe/entities/invoice/InvoiceLineId.java
@@ -3,11 +3,10 @@ package com.nguyen1207.vishbe.entities.invoice;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.util.UUID;
 
 @Data
 public class InvoiceLineId implements Serializable {
-    private UUID subInvoiceId;
+    private String subInvoiceId;
 
-    private UUID productId;
+    private String productId;
 }
--- src/main/java/com/nguyen1207/vishbe/entities/user/CartRowId.java
+++ src/main/java/com/nguyen1207/vishbe/entities/user/CartRowId.java
@@ -3,11 +3,10 @@ package com.nguyen1207.vishbe.entities.user;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.util.UUID;
 
 @Data
 public class CartRowId implements Serializable {
-    private UUID productId;
+    private String productId;
 
-    private UUID cartId;
+    private String cartId;
 }
